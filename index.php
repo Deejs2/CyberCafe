b529640a37d01ac950e4ea379469b31cd25f8883
Dhiraj <jireldhiraj123@gmail.com>
2024-03-14 06:59:43 +0545

    manage project

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $GLOBALS["menuLink"] = "?page=menu";
     <title>CyberCafe | <?php echo ucfirst($page)?></title>
     <link rel="stylesheet" href="design/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="design/css/style.css">
+    <link rel="stylesheet" href="design/css/landing.css">
 </head>
 <body>
 
